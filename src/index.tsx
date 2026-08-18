2c23eff757afb5d763831d3a8591fbca20ae2510
Tim Hays <timhays98@gmail.com>
2026-08-18 14:31:43 -0500

    Add analytics with goatcounter

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <script data-goatcounter="https://timhays.goatcounter.com/count"
+      async src="//gc.zgo.at/count.js"></script>
     <HashRouter>
       <App />
     </HashRouter>
